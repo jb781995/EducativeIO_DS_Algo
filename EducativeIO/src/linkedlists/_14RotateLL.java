71da92947aab649ff10daf8dddc7d1c112245735
jb781995 <bhardwajjsh95@gmail.com>
2021-11-22 19:04:14 -0600

    Linked List 22 NOV 2021
    _9UnionIntersectionLL_2
    _14RotateLL

---
+++ EducativeIO/src/linkedlists/_14RotateLL.java
@@ -0,0 +1,37 @@
+package linkedlists;
+
+public class _14RotateLL {
+
+private static Node head;
+private static int rotate;
+
+public static Node rotateLL(Node current) {
+	if(current.next==null)
+		return current;
+
+	Node node= rotateLL(current.next);
+	if(rotate>0) {
+	--rotate;
+    Node temp = head;
+	current.next.next = head;
+	head = node;
+	System.out.println("head: "+temp.data+", new head: "+head.data+", current.next.next: "+current.next.next.data+"\n");
+    current.next=null;
+
+	}
+	return current;
+}
+
+public static void main(String []a) {
+	Node node = new Node();
+	node.insert(1);
+	node.insert(2);
+	node.insert(3);
+	node.insert(4);
+	node.insert(5);
+	head = node.head;
+	rotate=3;
+	rotateLL(head);
+	node.print(head);
+}
+}
---
+++ EducativeIO/src/linkedlists/_9UnionIntersectionLL_2.java
@@ -0,0 +1,68 @@
+package linkedlists;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//import linkedlists._9UnionIntersectionLL.Node;
+
+public class _9UnionIntersectionLL_2 {
+   Node head;
+   
+	public static Node union(Node node1, Node node2) {
+		Node n1 = node1.getHead();
+		while(n1.next!=null) 
+			n1=n1.next;
+		
+		n1.next=node2.getHead();
+
+		removeDuplicates(node1);
+		
+		// can't return node1.head because it would then try to call print() on it
+		// which is equivalent of asking for 
+		// head.head which doens't exist. So, it won't print anything
+		return node1;
+	}
+	
+	public static void removeDuplicates(Node node) {
+		Set<Integer> uniqueNodes = new HashSet<>();
+		Node n = node.head;
+		uniqueNodes.add(n.data);
+
+		while(n.next!=null) {
+			if(!uniqueNodes.contains(n.next.data)) {
+				uniqueNodes.add(n.next.data);
+				n=n.next;
+			}
+			else {
+				n.next = n.next.next;
+			}
+		}
+	}
+	public static void main(String[] args) {
+		_9UnionIntersectionLL_2 o1 = new _9UnionIntersectionLL_2();
+		_9UnionIntersectionLL_2 o2 = new _9UnionIntersectionLL_2();
+		
+		Node node1= new Node();
+		node1.insert(15);
+		node1.insert(8);
+		node1.insert(14);
+		node1.insert(22);
+		node1.print(node1.getHead());
+		o1.head = node1.getHead();
+		
+		Node node2 = new Node();
+		node2.insert(7);
+		node2.insert(14);
+		node2.insert(22);
+		node2.insert(21);
+		System.out.println();
+		node2.print(node2.getHead());
+		o2.head = node2.getHead();
+		
+		System.out.println("\nUnion:");
+		System.out.println("****************************************");
+		union(node1, node2).print();
+
+	}
+
+}
